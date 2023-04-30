6c2ce9e61b2ddcda65bf95b24e9352df5265b037
Hakan Fırat <hakanfirat@Hakans-Mac-mini.local>
2023-04-30 09:05:53 +0300

    add routes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import {createRoot} from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 

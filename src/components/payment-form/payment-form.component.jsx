77ab64b482193350cb83c1fe1a9a628819e9fbe2
Hakan Fırat <hakanfirat@Hakans-Mac-mini.local>
2023-06-02 13:19:59 +0300

    update

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -21,7 +21,7 @@ const PaymentForm = () => {
 
         setIsProcessingPayment(true)
 
-        const response = await fetch('/.netlify/functions/create-payment-intent', {
+        const response = await fetch('/.vercel/functions/create-payment-intent', {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json'
